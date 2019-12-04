ceecfa8d939edddb604a1949a7524f0e6ccdb73b
Yaya  SADIO <ysadio@gainde2000.sn>
2019-12-04 12:48:56 +0000

    page one

---
+++ src/Components/Body.jsx
@@ -0,0 +1,187 @@
+/* eslint-disable jsx-a11y/iframe-has-title */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react'
+
+function Body(){
+    return(
+        <div className="TOC">
+            <ul className="social social-fixed">
+                <li><a href="#"><i className="socicon-twitter"></i></a></li>
+                <li><a href="#"><i className="socicon-facebook"></i></a></li>
+                <li><a href="#"><i className="socicon-googleplus"></i></a></li>
+            </ul>
+
+
+            <div className="horizontal-nav">
+                <div className="container">
+                    <ul id="menu">
+                        <li data-menuanchor="Home" className="active"></li>
+                        <li data-menuanchor="Services"></li>
+                        <li data-menuanchor="Projects"></li>
+                        <li data-menuanchor="Clients"></li>
+                        <li data-menuanchor="Contacts"></li>
+                    </ul>
+                </div>
+                <div className="horizontal-controls">
+                    <button className="a-pp-prev">Precedent</button>
+                    <span className="divider">|</span>
+                    <button className="a-pp-next">Suivant</button>
+                </div>
+            </div>
+
+            <div className="home-horizontal a-horizontal full-height">
+                <div className="section pp-scrollable slide slide1 slide-dark">
+                    <div className="slide-container">
+                        <div className="video-container a-video" data-vimeo-width="640" data-vimeo-height="360"><iframe src="https://player.vimeo.com/video/253112375?title=0&byline=0&portrait=0&autoplay=1&autopause=0&muted=1&background=1" allowFullScreen></iframe></div>
+                        <div className="slide-bg"><div className="inside"></div></div>
+                        <div className="slide-video-container slide-container">
+                            <div className="container">
+                                <div className="row slide-content">
+                                    <div className="col-sm-4">
+                                        <div className="slide-num num">01</div>
+                                        <h1 className="slide-title font-teko wow fadeInRight" data-wow-duration="1.2s"><span className="text-primary">Lewis</span><br /> Creativity<br /> Studio</h1>
+                                        <div className="slide-body wow fadeInRight" data-wow-delay=".2s" data-wow-duration="1.2s">
+                                            <h3 className="slide-title-sub font-teko">branding &amp; interaction</h3>
+                                            <div className="slide-descr">We create high quality products to help the life interested & better</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <button className="btn-play a-video-play"><i className="icon ion-ios-play"></i></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="section pp-scrollable slide slide2">
+                    <div className="slide-container">
+                        <div className="container">
+                            <div className="slide-num num">02</div>
+                            <div className="media-title">
+                                <div className="align-self-center">
+                                    <h2 className="slide-title font-teko mb-0">Our Services</h2>
+                                </div>
+                                <div className="align-self-center mb-2">
+                                    <div className="slide-descr slide-note">When unknow printer took a galley of type and scrambled it<br /> to make a type specimen book.</div>
+                                </div>
+                            </div>
+                            <div className="slide-body">
+                                <div className="row horizontal-service-list">
+                                    <div className="col-lg-4">
+                                        <div className="horizontal-service-item">
+                                            <div className="num">1.</div>
+                                            <div className="horizontal-service-item-photo"><img src="img/horizontal-pic2.png" alt="" className="photo-position" /></div>
+                                            <div className="horizontal-service-item-title">Graphic design &amp;&nbsp;Branding</div>
+                                            <div className="horizontal-service-item-descr text-grey">We build and activate brands throung cultural insight, strategic vision, and the power of emotion..</div>
+                                            <div className="horizontal-service-item-more"><a className="btn btn-primary btn-sm" href="service.html">Learn more <i className="icon ion-ios-arrow-right"></i></a></div>
+                                        </div>
+                                    </div>
+                                    <div className="col-lg-4">
+                                        <div className="horizontal-service-item">
+                                            <div className="num">2.</div>
+                                            <div className="horizontal-service-item-photo"><img src="img/horizontal-pic3.png" alt="" className="photo-position" /></div>
+                                            <div className="horizontal-service-item-title">ui/ux &amp;<br /> interaction</div>
+                                            <div className="horizontal-service-item-descr text-grey">We build and activate brands throung cultural insight, strategic vision, and the power of emotion..</div>
+                                            <div className="horizontal-service-item-more"><a className="btn btn-primary btn-sm" href="service.html">Learn more <i className="icon ion-ios-arrow-right"></i></a></div>
+                                        </div>
+                                    </div>
+                                    <div className="col-lg-4">
+                                        <div className="horizontal-service-item">
+                                            <div className="num">3.</div>
+                                            <div className="horizontal-service-item-photo photo3"><img src="img/horizontal-pic4.png" alt="" className="photo-position" /></div>
+                                            <div className="horizontal-service-item-title">animation<br /> &amp; motion</div>
+                                            <div className="horizontal-service-item-descr text-grey">We build and activate brands throung cultural insight, strategic vision, and the power of emotion..</div>
+                                            <div className="horizontal-service-item-more"><a className="btn btn-primary btn-sm" href="service.html">Learn more <i className="icon ion-ios-arrow-right"></i></a></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="section pp-scrollable slide slide3 slide-dark">
+                    <div className="slide-container">
+                        <div className="slide-bg"><div className="inside"></div></div>
+                        <div className="container">
+                            <div className="row slide-content">
+                                <div className="col-sm-4">
+                                    <div className="slide-num num">03</div>
+                                    <div className="slide-title">
+                                        <div className="horizontal-top-title">Featured project</div>
+                                        <h2 className="font-teko h1">Dark<br /> Bottle</h2>
+                                    </div>
+                                    <div className="slide-body">
+                                        <h3 className="slide-title-sub font-teko">branding</h3>
+                                    </div>
+                                </div>
+                            </div>
+                            <a className="btn btn-primary mr-4" href="single-project-branding.html">See project <i className="icon ion-ios-arrow-right"></i></a>
+                            <a href="work-grid.html" className="horizontal-more-total">all project</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="section pp-scrollable slide slide4">
+                    <div className="slide-container">
+                        <div className="container">
+                            <div className="slide-num num">04</div>
+                            <div className="media-title">
+                                <div className="align-self-center">
+                                    <h2 className="slide-title font-teko mb-0">Our Clients</h2>
+                                </div>
+                                <div className="align-self-center mb-2">
+                                    <div className="slide-descr slide-note">Help business can be optimized profit and improve value<br /> of brand is our vision.</div>
+                                </div>
+                            </div>
+
+                            <div className="slide-body">
+                                <div className="row no-gutters partner-list partner-bordered">
+                                    <div className="col partner-item"><span className="inside"><img src="img/pic12.png" alt="" /></span></div>
+                                    <div className="col partner-item"><a href="#" className="inside"><img src="img/pic13.png" alt="" /></a></div>
+                                    <div className="col partner-item"><a href="#" className="inside"><img src="img/pic14.png" alt="" /></a></div>
+                                    <div className="col partner-item"><a href="#" className="inside"><img src="img/pic15.png" alt="" /></a></div>
+                                    <div className="col partner-item"><a href="#" className="inside"><img src="img/pic16.png" alt="" /></a></div>
+                                    <div className="col partner-item"><a href="#" className="inside"><img src="img/pic17.png" alt="" /></a></div>
+                                    <div className="col partner-item"><a href="#" className="inside"><img src="img/pic18.png" alt="" /></a></div>
+                                    <div className="col partner-item"><a href="#" className="inside"><img src="img/pic20.png" alt="" /></a></div>
+                                    <div className="col partner-item"><a href="#" className="inside"><img src="img/pic21.png" alt="" /></a></div>
+                                    <div className="col partner-item"><a href="#" className="inside"><img src="img/pic22.png" alt="" /></a></div>
+                                    <div className="col partner-item"><a href="#" className="inside"><img src="img/pic23.png" alt="" /></a></div>
+                                    <div className="col partner-item"><a href="#" className="inside"><img src="img/pic24.png" alt="" /></a></div>
+                                    <div className="col partner-item"><a href="#" className="inside"><img src="img/pic25.png" alt="" /></a></div>
+                                    <div className="col partner-item"><a href="#" className="inside"><img src="img/pic26.png" alt="" /></a></div>
+                                    <div className="col partner-item"><a href="#" className="inside"><img src="img/pic27.png" alt="" /></a></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="section pp-scrollable slide slide5 slide-dark">
+                    <div className="slide-container">
+                        <div className="slide-bg"><div className="inside"></div></div>
+                        <div className="container">
+                            <div className="row slide-content">
+                                <div className="col-sm-6">
+                                    <div className="slide-num num">05</div>
+                                    <h2 className="slide-title font-teko h1"><span className="text-primary">New York,</span> USA</h2>
+                                    <div className="slide-body">
+                                        <div className="slide-contact-address">166 Main Street, Beverly Hills, CA 90210</div>
+                                        <div className="slide-contact-mail"><a href="#" className="underline">hello@lewisstudio.co</a></div>
+                                        <div className="slide-contact-phone"><a href="tel:+00853462188" className="phone-link">+0085 346 2188</a></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a className="btn btn-primary mr-4" href="contact.html">Contact us <i className="icon ion-ios-arrow-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+
+
+        </div>
+    );
+}
+export default Body;
